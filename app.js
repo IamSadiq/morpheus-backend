5f60a8476e11577103f632270e1608b3e3002b5c
Abu-Bakr Adam <asiddik5@gmail.com>
2018-10-21 12:47:27 +0100

    modified app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const loginAuth = require('./routes/auth/auth-login');
